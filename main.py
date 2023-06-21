d7c79f9030965f277eef010d41b351d36b6b8084
Константин <127137549+KurtovK@users.noreply.github.com>
2023-06-21 10:36:09 +0300

    Внес изменения

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ from abc import ABC, abstractmethod
 #принимающий в параметрах координаты начала и конца пути, а
 #возвращающий массив точек маршрута. Реализуйте класс навигатора,
 #который по переданной ему стратегии выполняет построение маршрута.
+from abc import ABC, abstractmethod
+class Point:
+    def __init__(self, point_x: int, point_y: int):
+        self.__point_x = point_x
+        self.__point_y = point_y
+
+    def __str__(self):
+        return f"{self.__point_x}, {self.__point_y}"
+
+
 class Strategy(ABC):
-    @abstractmethod
-    def calculate_route(self, start_point, end_point):
-        pass
-
-
-class StrategyDecorator(Strategy):
-    def __init__(self, strategy):
-        self._strategy = strategy
-
     @abstractmethod
     def calculate_route(self, start_point, end_point):
         pass
@@ -47,6 +48,27 @@ class PublicTransportStrategy(Strategy):
         pass
 
 
+class CyclingStrategy(Strategy):
+    def calculate_route(self, start_point, end_point):
+        # алгоритм поиска маршрута для велосипедистов
+        pass
+
+
+class MyCustomStrategy(Strategy):
+    def calculate_route(self, start_point, end_point):
+        # алгоритм поиска маршрута с учетом каких-то особых требований
+        pass
+
+
+class StrategyDecorator(Strategy):
+    def __init__(self, strategy):
+        self._strategy = strategy
+
+    @abstractmethod
+    def calculate_route(self, start_point, end_point):
+        pass
+
+
 class RouteLogger(StrategyDecorator):
     def __init__(self, strategy):
         super().__init__(strategy)
@@ -66,13 +88,48 @@ class Navigator:
         return route
 
     def set_strategy(self, strategy):
+        if not isinstance(strategy, Strategy):
+            raise TypeError("Стратегия должна быть объектом класса, наследующегося от Strategy.")
         self.strategy = strategy
+
+
+class BaseStrategy(ABC):
+    @abstractmethod
+    def build_route(self, start_point: Point, finish_point: Point):
+        raise NotImplementedError
+
+
+class CarStrategy(BaseStrategy):
+    def build_route(self, start_point: Point, finish_point: Point):
+        return f"Маршрут построен из точки {start_point} в точку {finish_point}. Вид транспорта: автомобиль."
+
+
+class PublicTransportStrategy2(BaseStrategy):
+    def build_route(self, start_point: Point, finish_point: Point):
+        return f"Маршрут построен из точки {start_point} в точку {finish_point}. Вид транспорта: городской транспорт."
+
+
+class WalkingStrategy2(BaseStrategy):
+    def build_route(self, start_point: Point, finish_point: Point):
+        return f"Маршрут построен из точки {start_point} в точку {finish_point}. Вид транспорта: пешком."
+
+
+class Navigator2:
+    __strategy = BaseStrategy
+
+    def set_strategy(self, strategy: BaseStrategy):
+        self.__strategy = strategy
+
+    def build_route(self, start_point: Point, finish_point: Point):
+        return self.__strategy.build_route(start_point, finish_point)
 
 
 def execute_application():
     walking_strategy = WalkingStrategy()
     driving_strategy = DrivingStrategy()
     public_transport_strategy = PublicTransportStrategy()
+    cycling_strategy = CyclingStrategy()
+    my_custom_strategy = MyCustomStrategy()
 
     decorated_driving_strategy = RouteLogger(driving_strategy)
 
@@ -80,9 +137,32 @@ def execute_application():
     start_point = (55.753960, 37.620393)  # координаты Красной площади в Москве
     end_point = (55.715230, 37.552450)  # координаты парка Горького в Москве
 
+    # Стратегия по умолчанию - RouteLogger
     route = navigator.build_route(start_point, end_point)
     print(route)  # выводим массив точек маршрута
-
-
+    
+    # Переключаем стратегию на WalkingStrategy
+    navigator.set_strategy(walking_strategy)
+    route = navigator.build_route(start_point, end_point)
+    print(route)
+    
+    # Переключаем стратегию на MyCustomStrategy
+    navigator.set_strategy(my_custom_strategy)
+    route = navigator.build_route(start_point, end_point)
+    print(route)
+    
+    # Пример использования второго набора классов
+    navigator2 = Navigator2()
+    start_point = Point(0, 0)
+    finish_point = Point(10, 10)
+    
+    navigator2.set_strategy(CarStrategy())
+    print(navigator2.build_route(start_point, finish_point))
+    
+    navigator2.set_strategy(WalkingStrategy2())
+    print(navigator2.build_route(start_point, finish_point))
+    
+    navigator2.set_strategy(PublicTransportStrategy2())
+    print(navigator2.build_route(start_point, finish_point))
 if __name__ == "__main__":
     execute_application()
