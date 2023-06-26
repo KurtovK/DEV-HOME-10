d044aa9437d28fa7aa79db64ae363cbf7c575f41
KKD <kossstyan87@mail.ru>
2023-06-26 09:17:58 +0300

    Внес изменения

--- data_structure/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, login: str, password: str):
-        self.__login = login
-        self.__password = password
-
-    def __str__(self):
-        return f" Логин: {self.__login}; Пароль: {self.__password}"
